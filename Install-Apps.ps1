c23a7b00a41288fc9509a40a31dbb3009cb17630
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2022-12-28 07:00:38 -0800

    Update install script.

--- Install-Apps.ps1
+++ Install-Apps.ps1
@@ -1,13 +1,24 @@
 . "$PSScriptRoot\Install-WinGetApp.ps1"
 
-Install-WinGetApp -AppName ImageMagick.ImageMagick
+Install-WinGetApp -AppName Microsoft.PowerShell
 Install-WinGetApp -AppName Git.Git
+Install-WinGetApp -AppName Notepad++.Notepad++
+
+Install-WinGetApp -AppName ImageMagick.ImageMagick
 Install-WinGetApp -AppName VideoLAN.VLC
 Install-WinGetApp -AppName Mp3tag.Mp3tag
 
+# .NET
+Install-WinGetApp -AppName icsharpcode.ILSpy -InstallFolder 'D:\Apps\ILSpy'
 
-Install-WinGetApp -AppName Kitware.CMake -InstallFolder 'D:\Apps\CMake'
-Install-WinGetApp -AppName Graphviz.Graphviz -InstallFolder 'D:\Apps\Graphviz'
+# VSC
 Install-WinGetApp -AppName Microsoft.VisualStudioCode -InstallFolder 'D:\Apps\Microsoft VS Code'
+
+# Security
+Install-WinGetApp -AppName KeePassXCTeam.KeePassXC -InstallFolder 'D:\Apps\KeePassXC'
 Install-WinGetApp -AppName GnuPG.GnuPG -InstallFolder 'D:\Apps\GnuPG'
 Install-WinGetApp -AppName GnuPG.Gpg4win -InstallFolder 'D:\Apps\Gpg4win'
+
+# Misc
+Install-WinGetApp -AppName Graphviz.Graphviz -InstallFolder 'D:\Apps\Graphviz'
+Install-WinGetApp -AppName Kitware.CMake -InstallFolder 'D:\Apps\CMake'
