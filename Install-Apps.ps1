ec19350280d6faca20fad5e3f4dba028951cf696
AjayKMehta <11180071+AjayKMehta@users.noreply.github.com>
2022-12-31 21:33:09 -0800

    Cleanup

--- Install-Apps.ps1
+++ Install-Apps.ps1
@@ -5,30 +5,39 @@ param (
     [string] $CustomLocation
 )
 
-Install-WinGetApp -AppName Microsoft.PowerShell
-Install-WinGetApp -AppName Git.Git
-Install-WinGetApp -AppName Notepad++.Notepad++
+if ($PSVersionTable.PSVersion -lt 7) {
+    Install-WinGetApp -AppName Microsoft.PowerShell
+}
 
-Install-WinGetApp -AppName ImageMagick.ImageMagick
-Install-WinGetApp -AppName VideoLAN.VLC
-Install-WinGetApp -AppName Mp3tag.Mp3tag
-Install-WinGetApp -AppName Citrix.Workspace
-Install-WinGetApp -AppName Postman.Postman
-
-# .NET
-Install-WinGetApp -AppName icsharpcode.ILSpy $CustomLocation 'ILSpy'
-Install-WinGetApp -AppName 'LINQPad.LINQPad.7' $CustomLocation 'LINQPad7'
-
-# VSC
-Install-WinGetApp -AppName Microsoft.VisualStudioCode $CustomLocation 'Microsoft VS Code'
+# CLI apps
+Install-WinGetApp -AppName JohnTaylor.less -l $CustomLocation 'CLI'
+Install-WinGetApp -AppName JohnTaylor.lesskey -l $CustomLocation 'CLI'
+exe
+Install-WinGetApp -AppName yq $CustomLocation 'CLI' '-r' yq.exe
 
 # Security
 Install-WinGetApp -AppName KeePassXCTeam.KeePassXC $CustomLocation 'KeePassXC'
 Install-WinGetApp -AppName GnuPG.GnuPG $CustomLocation 'GnuPG'
 Install-WinGetApp -AppName GnuPG.Gpg4win $CustomLocation 'Gpg4win'
 
-# Misc
+# Dev
+Install-WinGetApp -AppName Git.Git
+Install-WinGetApp -AppName Microsoft.VisualStudioCode $CustomLocation 'Microsoft VS Code'
 Install-WinGetApp -AppName OpenJS.NodeJS $CustomLocation 'nodejs'
-Install-WinGetApp -AppName Graphviz.Graphviz $CustomLocation 'Graphviz'
 Install-WinGetApp -AppName Kitware.CMake $CustomLocation 'CMake'
+Install-WinGetApp -AppName Notepad++.Notepad++
+
+# Media
+Install-WinGetApp -AppName ImageMagick.ImageMagick
+Install-WinGetApp -AppName VideoLAN.VLC
+Install-WinGetApp -AppName Mp3tag.Mp3tag
+
+# .NET
+Install-WinGetApp -AppName icsharpcode.ILSpy $CustomLocation 'ILSpy'
+Install-WinGetApp -AppName 'LINQPad.LINQPad.7' $CustomLocation 'LINQPad7'
+
+# Misc
+Install-WinGetApp -AppName Graphviz.Graphviz $CustomLocation 'Graphviz'
 Install-WinGetApp -AppName WinSCP.WinSCP $CustomLocation 'WinSCP'
+Install-WinGetApp -AppName Citrix.Workspace
+Install-WinGetApp -AppName Postman.Postman
